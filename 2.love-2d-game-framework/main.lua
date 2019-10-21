19c4ce5f3ff3f7776f53bfb0c3b74e876668bb58
jskyzero <jskyzero@outlook.com>
2019-10-22 01:17:26 +0800

    add build  powershell cmd

--- 2.love-2d-game-framework/main.lua
+++ 2.love-2d-game-framework/main.lua
@@ -1,28 +1,10 @@
 -- main of lua game
 
-
--- Sets the filesystem paths that will be searched when require is called.
-love.filesystem.setRequirePath("src/?.lua")
+-- sets the filesystem paths that will be searched when require is called.
+love.filesystem.setRequirePath("src/?.lua;src/?/?.lua")
 
 -- import module
-local system = require("system")
+local system = require("core.system")
 
-
-
-
-
-
--- love.load to do one-time setup of your game
-function love.load()
-  system.initial()
-end
-
--- love.update which is used to manage your game's state frame-to-frame
-function love.update(dt)
-  system.update(dt)
-end
-
--- love.draw which is used to render the game state onto the screen.
-function love.draw()
-  system.render()
-end
+-- run main
+system.main()
---
+++ 2.love-2d-game-framework/src/core/system.lua
@@ -0,0 +1,51 @@
+-- core.system
+-- use to link love callbacks with core logic
+
+system = {}
+
+local x, y, w, h = 20, 20, 60, 20
+
+-- main entry for link
+function system.main()
+  -- love use callbacks
+
+  -- love.load to do one-time setup of your game
+  function love.load()
+    system.initial()
+  end
+
+  -- love.update which is used to manage your game's state frame-to-frame
+  function love.update(dt)
+    system.update(dt)
+  end
+
+  -- love.draw which is used to render the game state onto the screen.
+  function love.draw()
+    system.render()
+  end
+
+  function love.run()
+    return system.run()
+  end
+end  
+
+
+system.run = require((...) .. "Run")
+
+function system.initial()
+  local major, minor, revision, codename = love.getVersion()
+  local str = string.format("Version %d.%d.%d - %s", major, minor, revision, codename)
+  -- print(str)
+end
+
+function system.update(dt)
+  w = w + 1
+  h = h + 1
+end
+
+function system.render()
+  love.graphics.setColor(0, 0.4, 0.4)
+  love.graphics.rectangle("fill", x, y, w, h)
+end
+
+return system
